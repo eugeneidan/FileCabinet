df00ddb6c6370f25d96bba0508d386e88d757f79
Eugene Idan <eugeneidan@gmail.com>
2016-07-21 01:03:04 +0000

    Added Devise to the application

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -24,13 +24,19 @@ class DocsController < ApplicationController
 	end
 
 	def edit
-
 	end
 
 	def update
+		if @doc.update(doc_params)
+			redirect_to @doc, notice: 'Document Saved!'
+		else
+			render :edit
+		end
 	end
 
 	def destroy
+		@doc.destroy
+		redirect_to docs_path
 	end
 
 	private
