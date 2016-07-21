191a34c2016bf3bda38efa17d21432f1ab7bb4b1
Eugene Idan <eugeneidan@gmail.com>
2016-07-21 01:16:53 +0000

    changed root of application for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'welcome#index'
 
   resources :docs
+  authenticated :user do 
+  	root 'docs#index', as: 'authenticated_root'
+  end
+
+  root 'welcome#index'
 
 end
